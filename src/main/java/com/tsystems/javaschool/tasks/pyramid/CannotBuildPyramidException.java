207aa5c6e862864744c9faa1f18575c3967e10e9
Jarek <jarekpavlov@gmail.com>
2020-11-25 20:13:34 +0300

    Pyramid task is finished

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
@@ -1,4 +1,9 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import com.sun.org.apache.xerces.internal.impl.dv.xs.BaseDVFactory;
+
 public class CannotBuildPyramidException extends RuntimeException {
+
+
+
 }
--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -13,8 +14,37 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+
+        if (inputNumbers == null) throw new CannotBuildPyramidException();
+        for (int i = 0; i < inputNumbers.size(); i++)
+            if (inputNumbers.get(i) == null)
+                throw new CannotBuildPyramidException();
+
+        double triangNum = (Math.sqrt(8 * inputNumbers.size() + 1) - 1) / 2;
+        if (triangNum % 1 != 0)
+            throw new CannotBuildPyramidException();
+
+        int[][] pyramid = new int[(int)triangNum][(int)triangNum * 2 - 1];
+        for (int i = 0; i < pyramid.length; i++)
+            for (int j = 0; j < pyramid[i].length; j++)
+                pyramid[i][j] = 0;
+
+        Collections.sort(inputNumbers);
+
+        int currentElemNum = 0;
+        for (int i = 0; i < pyramid.length; i++) {
+            int j = (int)triangNum - i - 1;
+            int numbersInARow = i + 1;
+            while (numbersInARow > 0) {
+                pyramid[i][j] = inputNumbers.get(currentElemNum);
+                currentElemNum++;
+                numbersInARow--;
+                j += 2;
+            }
+        }
+
+        return pyramid;
+
     }
 
 
